a10187fe1eb8e3384b8d7b4e0153a462922590d2
Brett Kinnamon <brettkinnamon@Bretts-MacBook-Pro.local>
2020-10-18 12:31:45 -0400

    invalid vnode with external slot

---
+++ consumer/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <MyButton>{{ text }}</MyButton>
+</template>
+
+<script>
+import MyButton from "../../component-library/src/components/MyButton.vue";
+
+export default {
+  components: {
+    MyButton
+  },
+  setup() {
+    const text = "Button Text";
+    return {
+      text
+    };
+  }
+};
+</script>
